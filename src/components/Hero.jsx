9711c1fc7498d71ef25c39cbac8f29678701a2c3
Tandou Sall <tandousall@gmail.com>
2026-03-11 16:15:25 +0000

    fixed images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ return ( <section className="w-full flex justify-center bg-black py-12 px-12">
   <img
     src="/banner.jpg"
     alt="La Marea Banner"
-    className="w-full h-[950px] object-cover shadow-2xl"
+    className="w-full h-auto object-contain"
   />
 
 </section>
