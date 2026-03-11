41f98b4dc538ccbb653dce0e61cd656249b47845
Tandou Sall <tandousall@gmail.com>
2026-03-11 16:20:55 +0000

    fixed features

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,17 @@
 function Hero() {
-return ( <section className="w-full flex justify-center bg-black py-12 px-12">
-
+return ( <section className="w-full flex justify-center bg-black py-12 px-4 md:px-12">
 
   <img
     src="/banner.jpg"
     alt="La Marea Banner"
-    className="w-full h-auto object-contain"
+    className="w-full max-w-6xl h-auto object-contain"
   />
 
 </section>
+
 )
 }
 
 export default Hero
+
 
